fb44669725a75a26eb95f6e16e8220f859517e6c
flopezchaves <facuchaves@gmail.com>
2021-04-17 13:54:00 -0300

    Add redis cache with nestjs annotations

--- src/player/player.module.ts
+++ src/player/player.module.ts
@@ -4,13 +4,18 @@ import { PlayerService } from './player.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Player } from './entities/player.entity';
 import { Repository } from 'typeorm';
-import { APP_INTERCEPTOR } from '@nestjs/core';
+import * as redisStore from 'cache-manager-redis-store';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([Player]),
     Repository,
-    CacheModule.register()
+    //CacheModule.register()
+    CacheModule.register({
+      store: redisStore,
+      host: 'localhost',
+      port: 6379,
+    })
   ],
   controllers: [PlayerController],
   providers: [
