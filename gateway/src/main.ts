5d03a8f99e154274d0c1d3e79166ab5215e94ca1
facu <facuchaves@gmailcom>
2022-01-28 11:44:03 -0300

    Change

--- gateway/src/main.ts
+++ gateway/src/main.ts
@@ -32,7 +32,7 @@ async function bootstrap() {
   await app.startAllMicroservicesAsync();
 
   const config = new DocumentBuilder()
-  .setTitle('CRUDX Example')
+  .setTitle('CRUDX Example e')
   .setDescription('This is a litlle but very complete CRUDX for an generic entity.')
   .setVersion('1.0')
   .addTag('Recourses')
