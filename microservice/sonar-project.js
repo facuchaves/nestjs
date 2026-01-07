919014781a22a4fb779ff4169da708d6021d32e1
Facundo Chaves <facuchaves@gmail.com>
2026-01-07 11:54:02 -0300

    Sonarqube (#70)
    * add dependencies and initial config for sonarqube
    
    * fix coverage on jest + integration with sonarquve coverage
    
    ---------
    
    Co-authored-by: facu <facuchaves@gmailcom>

---
+++ microservice/sonar-project.js
@@ -0,0 +1,16 @@
+const sonarqubeScanner =  require('sonarqube-scanner');
+sonarqubeScanner(
+    {
+        serverUrl:  'http://localhost:9000',
+        options : {
+            'sonar.sources':  'src',
+            'sonar.tests':  'test',
+            'sonar.inclusions'  :  '**', // Entry point of your code
+            'sonar.test.inclusions':  'test/**/*.spec.ts',
+            'sonar.javascript.lcov.reportPaths':  'coverage/lcov.info',
+            'sonar.testExecutionReportPaths':  'coverage/test-reporter.xml',
+            'sonar.login':  'admin',
+            'sonar.password':  'sonarqube1234',
+            'sonar.coverage.exclusions': 'src/main.ts,src/app.module.ts',
+        }
+    }, () => {});
