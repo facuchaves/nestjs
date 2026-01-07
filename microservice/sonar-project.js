53e3faf42f3199b905474e9c9e97a089f1a48d68
facu <facuchaves@gmailcom>
2026-01-07 11:20:17 -0300

    fix coverage on jest + integration with sonarquve coverage

--- microservice/sonar-project.js
+++ microservice/sonar-project.js
@@ -4,12 +4,13 @@ sonarqubeScanner(
         serverUrl:  'http://localhost:9000',
         options : {
             'sonar.sources':  'src',
-            'sonar.tests':  'src',
+            'sonar.tests':  'test',
             'sonar.inclusions'  :  '**', // Entry point of your code
-            'sonar.test.inclusions':  'src/**/*.spec.ts,src/**/*.spec.jsx,src/**/*.test.js,src/**/*.test.jsx',
+            'sonar.test.inclusions':  'test/**/*.spec.ts',
             'sonar.javascript.lcov.reportPaths':  'coverage/lcov.info',
-            //'sonar.testExecutionReportPaths':  'coverage/test-reporter.xml',
+            'sonar.testExecutionReportPaths':  'coverage/test-reporter.xml',
             'sonar.login':  'admin',
-            'sonar.password':  'sonarqube1234'
+            'sonar.password':  'sonarqube1234',
+            'sonar.coverage.exclusions': 'src/main.ts,src/app.module.ts',
         }
     }, () => {});
