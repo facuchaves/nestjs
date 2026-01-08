a8cddba646e4e6d7536020e87fff644fa2e0ac47
facu <facuchaves@gmailcom>
2026-01-08 12:14:39 -0300

    Change locations of tests

--- gateway/test/integration/entity.controller.spec.ts
+++ gateway/test/integration/entity.controller.spec.ts
@@ -2,22 +2,20 @@ import { INestApplication } from '@nestjs/common';
 import { ClientProxy, ClientsModule } from '@nestjs/microservices';
 import { Test, TestingModule } from '@nestjs/testing';
 import { Observable } from 'rxjs';
-import { EntityDto } from './dtos/entity.dto';
+import { EntityDto } from '../../src/entity/dtos/entity.dto';
 // import { GenericEntity } from './entities/generic-entity.entity';
-import { EntityController } from './entity.controller';
-import { EntityModule } from './entity.module';
-import { EntityService } from './entity.service';
+import { EntityController } from '../../src/entity/entity.controller';
+import { EntityModule } from '../../src/entity/entity.module';
+import { EntityService } from '../../src/entity/entity.service';
 import * as request from 'supertest';
 
 describe('Entity controller', () => {
-
   let app: INestApplication;
 
   beforeAll(async () => {
     const moduleRef: TestingModule = await Test.createTestingModule({
-      imports:[EntityModule],
-    })
-    .compile();
+      imports: [EntityModule],
+    }).compile();
 
     app = moduleRef.createNestApplication();
     await app.init();
@@ -28,19 +26,16 @@ describe('Entity controller', () => {
   // })
 
   describe('Happy paths', () => {
-
     it.skip(`/GET resources: should return all resources`, async () => {
       return request(app.getHttpServer())
         .get('/api/resource/local')
-        .expect(200)
-        // .expect(await entityService.getAllEntities(),
-        // );
+        .expect(200);
+      // .expect(await entityService.getAllEntities(),
+      // );
     });
-    
   });
 
   afterAll(async () => {
     await app.close();
   });
-
 });
