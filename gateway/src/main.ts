b2cbc401e162e5571e83e4ba7ef61a4d40a339e7
facu <facuchaves@gmailcom>
2026-01-19 17:28:14 -0300

    Fix node version + log sha version on bootstrap

--- gateway/src/main.ts
+++ gateway/src/main.ts
@@ -7,18 +7,18 @@ import { SecuritySchemeObject } from '@nestjs/swagger/dist/interfaces/open-api-s
 import * as session from 'express-session';
 import * as cookieParser from 'cookie-parser';
 import { Logger } from '@nestjs/common';
-const fs = require('fs')
+const fs = require('fs');
 declare const module: any;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule,{
+  const app = await NestFactory.create(AppModule, {
     logger: true,
     // httpsOptions: {
     //   cert: fs.readFileSync('../server.cert'),
     //   key: fs.readFileSync('../server.key')
     // }
   });
-  
+
   app.connectMicroservice({
     transport: Transport.TCP,
     options: {
@@ -32,20 +32,25 @@ async function bootstrap() {
   await app.startAllMicroservicesAsync();
 
   const config = new DocumentBuilder()
-  .setTitle('CRUDX Example')
-  .setDescription('This is a litlle but very complete CRUDX for an generic entity.')
-  .setVersion('1.0')
-  .addTag('Recourses')
-  // .addServer('https://')
-  // .addServer('http://localhost:8080/')
-  .addCookieAuth('x-token',{ name: 'x-token', description:'here is a description, maybe can give a default value' } as SecuritySchemeObject)
-  .build();
+    .setTitle('CRUDX Example')
+    .setDescription(
+      'This is a litlle but very complete CRUDX for an generic entity.',
+    )
+    .setVersion('1.0')
+    .addTag('Recourses')
+    // .addServer('https://')
+    // .addServer('http://localhost:8080/')
+    .addCookieAuth('x-token', {
+      name: 'x-token',
+      description: 'here is a description, maybe can give a default value',
+    } as SecuritySchemeObject)
+    .build();
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api/swagger', app, document);
 
   app.enableCors();
-  
+
   app.use(
     session({
       secret: 'my-secret',
@@ -57,18 +62,18 @@ async function bootstrap() {
 
   app.use(cookieParser());
 
-  app.useLogger( new Logger() )
+  app.useLogger(new Logger());
   const PORT = process.env.PORT || 8080;
 
   await app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}...`);
+    console.log(`Service version: ${process.env.APP_VERSION || 'v1.0.0'}`);
   });
 
   if (module.hot) {
     module.hot.accept();
     module.hot.dispose(() => app.close());
   }
-
 }
 
 bootstrap();
