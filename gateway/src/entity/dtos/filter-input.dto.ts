3bb871a274115fe34544005f116c56ec559bef92
facu <facuchaves@gmailcom>
2026-01-20 10:20:30 -0300

    Prettier

--- gateway/src/entity/dtos/filter-input.dto.ts
+++ gateway/src/entity/dtos/filter-input.dto.ts
@@ -1,27 +1,26 @@
-import { ApiProperty } from "@nestjs/swagger"
-import { IsInt, IsString, Max, Min } from "class-validator"
+import { ApiProperty } from '@nestjs/swagger';
+import { IsInt, IsString, Max, Min } from 'class-validator';
 
 export class FilterInputDto {
-    
-    @ApiProperty({
-        description: 'Min age filter',
-        type: Number,
-        minimum: 0,
-        examples: [18,27,53],
-        example: 21
-    })
-    @IsInt()
-    @Min(0)
-    minAge: number
+  @ApiProperty({
+    description: 'Min age filter',
+    type: Number,
+    minimum: 0,
+    examples: [18, 27, 53],
+    example: 21,
+  })
+  @IsInt()
+  @Min(0)
+  minAge: number;
 
-    @ApiProperty({
-        description: 'Max age filter',
-        type: Number,
-        minimum: 0,
-        examples: [19,28,54],
-        example: 71
-    })
-    @IsInt()
-    @Min(0)
-    maxAge: number
+  @ApiProperty({
+    description: 'Max age filter',
+    type: Number,
+    minimum: 0,
+    examples: [19, 28, 54],
+    example: 71,
+  })
+  @IsInt()
+  @Min(0)
+  maxAge: number;
 }
--- gateway/src/middlewares/roles.middleware.ts
+++ gateway/src/middlewares/roles.middleware.ts
@@ -5,8 +5,8 @@ import { Request, Response, NextFunction } from 'express';
 export class RolesMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
     console.log('RolesMiddleware');
-    req['user'] = { name: "UserNameReq" }
-    res['user'] = { name: "UserNameRes" }
+    req['user'] = { name: 'UserNameReq' };
+    res['user'] = { name: 'UserNameRes' };
     next();
   }
 }
